b7564da2550bb92fa8a0fd43da816e2e7f6b783a
Lukas <dudeklukasz10@gmail.com>
2021-04-10 20:22:38 +0200

    structur, init commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import Home from './components/Home';
+import Navigation from './components/Navigation';
+
+
+function App() {
+  return (
+    <div className="App">
+  <Router>
+    <>
+    <Navigation />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </>
+  </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import {NavLink} from "react-router-dom";
+
+const MENU_URL = "http://localhost:3001/menu"
+
+const Navigation = () => {
+    const [menuItems, SetMenuItems] = useState(false);
+
+    useEffect(()=>{
+        fetch(MENU_URL)
+        .then(res => res.json())
+        .then(data => SetMenuItems(data))
+        .catch(err => console.warn(err))
+    }, [])
+    console.log(menuItems);
+
+    return menuItems ? <>
+    <ul>
+        {menuItems.map((el) => <li key={el.id}>
+            <NavLink to={el.link} activeClassName="active">{el.name}</NavLink>
+        </li>
+        )}
+    </ul>
+    </> :
+    <>
+    <h1> Loading... </h1>
+    </>
+}
+
+export default Navigation;
